fe2a2e2dde46a48d52e2f01883c1cdc29c9e17fb
Lakindu Gunasekara <lakindugunasekara@Lakindus-MacBook-Air.local>
2018-02-18 10:30:36 +0530

    Adding the application

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+import { User } from '../_models/index';
+import { UserService } from '../_services/index';
+import { environment } from '../../environments/environment';
+
+@Component({
+    moduleId: module.id.toString(),
+    templateUrl: 'home.component.html'
+})
+
+export class HomeComponent implements OnInit {
+    currentUser: User;
+    users: User[] = [];
+
+    constructor(private userService: UserService) {
+        this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    }
+
+    ngOnInit() {
+        this.loadAllUsers();
+
+        (<any>window).Intercom('boot', {
+            app_id: environment.APP_ID,
+            name: this.currentUser.firstName + ' ' + this.currentUser.lastName,
+            email: this.currentUser.username,
+            });
+    }
+
+    deleteUser(id: number) {
+        this.userService.delete(id).subscribe(() => { this.loadAllUsers()});
+    }
+
+    private loadAllUsers() {
+        this.userService.getAll().subscribe(users => { this.users = users; });
+    }
+}
